dba70b52c07b2f5ed94b4fe867f75472cae91a3f
dejana023 <dejana023@gmail.com>
2023-03-16 14:44:05 +0100

    Updated

--- CoffeeShop/Interfaces/ICalculator.cs
+++ CoffeeShop/Interfaces/ICalculator.cs
@@ -3,5 +3,9 @@
     public interface ICalculator
     {
         public decimal CalculateTotalPrice(decimal price);
+
+        public decimal CalculateDiscount(decimal Discount);
+
+        public decimal CalculateIncrease(decimal Increase)
     }
 }
